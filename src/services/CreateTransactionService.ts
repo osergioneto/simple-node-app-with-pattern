018a438f10cbb04e49363cca26dc60260e165a99
Sérgio Neto <sergio.deusdedith@cubos.io>
2020-05-04 01:52:49 -0300

    feat:  add service to create transaction. add hasFund to validate client balance

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  private hasFund(value: number): void {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (value > total) {
+      throw Error("You can't create a outcome without a valid balance.");
+    }
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (type === 'outcome') {
+      this.hasFund(value);
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
